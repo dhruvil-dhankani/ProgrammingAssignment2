b941ecdccaffe12b3f3d0f63e85105010265fa27
dhruvil-dhankani <dhruvil619@gmail.com>
2015-06-21 16:56:59 +0530

    R Code updated

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
+## Below Functon checks if the prior calculated value exists or not and the decide to return value from cache or calculate inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ m <- NULL
+     set <- function(y) {
+         x <<- y
+         m <<- NULL
+     }
+     get <- function() x
+     setinverse <- function(inverse) m <<- inverse
+     getinverse <- function() m
+     list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## Below Functon actually calculate the inverse of SQUARE Matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		  m <- x$getinverse()
+     if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+     }
+     data <- x$get()
+     m <- solve(data, ...)
+     x$setinverse(m)
+     m
+		
 }
